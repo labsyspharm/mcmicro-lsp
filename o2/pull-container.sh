f4bcd0fa201ba228e3b7c7b8c832f2c0dc3df104
Jeremy Muhlich <jmuhlich@bitflood.org>
2023-05-26 20:45:00 -0400

    Add script to pull dockerhub containers to cache dir
    This is a workaround for the issue where newer docker containers built
    with github actions can't be read by the older version of singularity
    installed globally on O2. This script uses a newer version of
    singularity in a container (!) to run the "singularity pull" command.

---
+++ o2/pull-container.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -euo pipefail
+
+show_usage() {
+    cat <<EOF
+Usage: $(basename "$0") docker://organization/container:tag
+EOF
+}
+
+if [ $# -ne 1 ]; then
+    show_usage
+    echo
+    echo "Downloads a docker container into the mcmicro container cache directory"
+    exit 1
+fi
+
+url="$1"
+# Validate url format up front to simplify parsing later.
+if [ -z $(echo "$url" | grep -P '^docker://[^/ ]+/[^/ ]+:[^ ]+$' || true) ]; then
+    echo "Invalid dockerhub URI"
+    show_usage
+    exit 1
+fi
+org_cont_tag=${url:9}
+org=${org_cont_tag/\/*/}
+cont_tag=${org_cont_tag/*\//}
+cont=${cont_tag/:*/}
+tag=${cont_tag/*:/}
+out_path=/n/groups/lsp/mcmicro/singularity/$org-$cont-$tag.img
+
+echo "Container: $url"
+echo "Destination: $out_path"
+
+simg="/n/groups/lsp/mcmicro/singularity/singularity-3.11.0.img"
+cmd="singularity run -B /n/groups $simg pull $out_path $url"
+sg HITS_lsp-analysis "$cmd"
