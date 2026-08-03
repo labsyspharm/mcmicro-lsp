3c217c27f0b13bd74390d0825556a874c744b18b
Jeremy Muhlich <jmuhlich@bitflood.org>
2026-08-03 15:26:07 -0400

    Improve submit*single scripts

--- o2/submit-mccellpose-single.sh
+++ o2/submit-mccellpose-single.sh
@@ -6,15 +6,23 @@
 #SBATCH --mem=5g
 #SBATCH --gres=gpu:1
 
-SAMPLE_ID="$1"
-WORKDIR="$PWD"
+in_path=$(realpath -e $1)
+in_dir=$(dirname $in_path)
+workdir=$(mktemp -d -p $in_dir)
+fname=$(basename $in_path)
+sample_id=${fname%.ome.tif}
+
+echo "Working directory: $workdir"
 
 apptainer exec \
   --no-home \
   --pid \
-  -B "$WORKDIR" \
+  -B "$workdir" \
   --nv \
   --env CUDA_VISIBLE_DEVICES="$CUDA_VISIBLE_DEVICES" \
   /n/groups/lsp/mcmicro/singularity/docker.io-labsyspharm-mccellpose-1.0.3.img \
   /bin/bash \
-  -c "cd $WORKDIR; mccellpose -i $SAMPLE_ID.ome.tif -o $SAMPLE_ID-cellpose-cell.ome.tif --output-nucleus $SAMPLE_ID-cellpose-nucleus.ome.tif -c 1 --expand-size 2 --use-gpu --jobs 2"
+  -c "cd $workdir; mccellpose -i ${in_path} -o ${in_dir}/${sample_id}-cellpose-cell.ome.tif --output-nucleus ${in_dir}/${sample_id}-cellpose-nucleus.ome.tif -c 1 --expand-size 2 --use-gpu --jobs 2"
+
+rm -r $workdir
+echo "Removed working directory"
--- o2/submit-mcquant-single.sh
+++ o2/submit-mcquant-single.sh
@@ -4,13 +4,20 @@
 #SBATCH -t 0-8
 #SBATCH --mem=128g
 
-SAMPLE_ID="$1"
-WORKDIR="$PWD"
+in_path=$(realpath -e $1)
+workdir=$(dirname $in_path)
+fname=$(basename $in_path)
+sample_id=${fname%.ome.tif}
 
 apptainer exec \
   --no-home \
   --pid \
-  -B "$WORKDIR" \
+  -B "$workdir" \
   /n/groups/lsp/mcmicro/singularity/labsyspharm-quantification-1.6.0.img \
   /bin/bash \
-  -c "cd $WORKDIR; mcquant --image ${SAMPLE_ID}.ome.tif --masks ${SAMPLE_ID}-cellpose-{cell,nucleus}.ome.tif --channel_names markers.csv --output ."
+  -c "cd $workdir; mcquant --image ${fname} --masks ${sample_id}-cellpose-{cell,nucleus}.ome.tif --channel_names markers.csv --output ."
+
+cd ${workdir}
+for c in cell nucleus; do
+  mv ${sample_id}_${sample_id}-cellpose-${c}.csv ${sample_id}-cellpose-${c}.csv
+done
