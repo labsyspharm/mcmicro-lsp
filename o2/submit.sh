1036fb6df79aea3d899d35d871ab721670f3d521
Jeremy Muhlich <jmuhlich@bitflood.org>
2026-08-03 15:23:57 -0400

    Add comments and central report collection

--- o2/submit.sh
+++ o2/submit.sh
@@ -5,21 +5,31 @@
 #SBATCH --mem=2G
 #SBATCH --mail-type=END
 
+# Verify that the user-specified directory is indeed an mcmicro project.
 in="${1:-$(pwd)}"
 if [ ! -e "$in/markers.csv" ]; then
   echo "ERROR: $0: Input directory '$in' doesn't look like an mcmicro 1.0 project directory (markers.csv was not found there)" >&2
   exit 1;
 fi
 
+# Load the java module which is required for nextflow.
 module purge
 module load java
 
+# Limit nextflow memory to 1 GB.
 export NXF_JVM_ARGS=-Xmx1g
+# Run the latest version of nextflow that mcmicro 1.0 is compatible with.
 export NXF_VER=25.10.7
+# Set the workdir to a standard location in the user's scratch directory.
 export NXF_WORK=/n/scratch/users/"${USER:0:1}/$USER"/nextflow-work
 
+# Run the pipeline
 echo "Launching mcmicro in $in"
 echo
 cd "$in"
-
 nextflow run labsyspharm/mcmicro -profile O2LSP --in .
+
+# Make a copy of the nextflow reports for future planning.
+if [ -e pipeline_info/execution_trace_*.txt ]; then
+  cp -r pipeline_info /n/groups/lsp/mcmicro/reports/$USER-$(date -Iseconds)
+fi
